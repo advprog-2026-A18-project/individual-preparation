1875baed6e8bc9bf897298966832579520b2b28d
evanhwz <evan.haryo@ui.ac.id>
2026-02-13 09:31:26 +0700

    feat: implement exponent test and skeleton for other methods in arithmetic util

---
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
@@ -0,0 +1,50 @@
+package com.example.individualprep.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArithmeticUtilityTest {
+    private final ArithmeticUtility arithmeticUtility = new ArithmeticUtility();
+    private final double precision = 0.0001;
+
+    @Test
+    void testAdd(){
+        // TODO: implement
+    }
+
+    @Test
+    void testSubtract(){
+        // TODO: implement
+    }
+
+    @Test
+    void testMultiply(){
+        // TODO: implement
+    }
+
+    @Test
+    void testDivideNormal(){
+        // TODO: implement division not by zero
+    }
+
+    @Test
+    void testDivideByZero(){
+        // TODO: implement division by zero
+    }
+
+    @Test
+    void testExponentPositive(){
+        assertEquals(8.0, arithmeticUtility.exponent(2.0, 3), precision);
+    }
+
+    @Test
+    void testExponentZero(){
+        assertEquals(1.0, arithmeticUtility.exponent(2.0, 0), precision);
+    }
+
+    @Test
+    void testExponentNegative(){
+        assertEquals(0.5, arithmeticUtility.exponent(2.0, -1), precision);
+    }
+
+}
